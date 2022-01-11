ca3fda83c8bb8ca741db68defc9d6e8e6706996c
Nguyễn Công Danh <ncdanh19@gmail.com>
2022-01-12 01:14:58 +0700

    Chathub real time

--- ChatHubDemo/Startup.cs
+++ ChatHubDemo/Startup.cs
@@ -1,3 +1,4 @@
+using ChatHubDemo.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,8 @@ namespace ChatHubDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +54,11 @@ namespace ChatHubDemo
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
             });
         }
     }
